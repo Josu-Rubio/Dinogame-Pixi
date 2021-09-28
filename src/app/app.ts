9e66a92f1388c194c8491a0038438c0f9ba716f1
Josué Rubio <josu_mr@hotmail.com>
2021-09-28 16:25:18 +0200

    creating game controllers

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,59 +1,56 @@
-import { bomberFrames } from '../assets/loader';
-import * as PIXI from 'pixi.js';
-
-interface BomberFrames {
-    front: string[];
-    back: string[];
-    right: string[];
-    left:  string[];
-}
-
-// Prepare frames
-const playerFrames: BomberFrames = bomberFrames;
-
-// IMPORTANT: Change this value in order to see the Hot Module Reloading!
-const currentFrame: keyof BomberFrames = 'front';
-
+import PIXI = require('pixi.js');
 
 export class GameApp {
+  private app: PIXI.Application;
 
-    private app: PIXI.Application;
+  static GameOver: boolean = false;
+  static PressedSpace: boolean = false;
+  static Score: number = 0;
 
-    constructor(parent: HTMLElement, width: number, height: number) {
+  constructor(parent: HTMLElement, width: number, height: number) {
+    this.app = new PIXI.Application({
+      width,
+      height,
+      backgroundColor: 0xffffff,
+      antialias: false,
+      resolution: 3,
+    });
 
-        this.app = new PIXI.Application({width, height, backgroundColor : 0x000000});
-        parent.replaceChild(this.app.view, parent.lastElementChild); // Hack for parcel HMR
+    PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 
-        // init Pixi loader
-        let loader = new PIXI.Loader();
+    // init Pixi loader
+    let loader = new PIXI.Loader();
 
-        // Add user player assets
-        console.log('Player to load', playerFrames);
-        Object.keys(playerFrames).forEach(key => {
-            loader.add(playerFrames[key]);
-        });
+    // Add user player assets
+    console.log('Player to load', playerFrames);
+    Object.keys(playerFrames).forEach((key) => {
+      loader.add(playerFrames[key]);
+    });
 
-        // Load assets
-        loader.load(this.onAssetsLoaded.bind(this));
+    // Load assets
+    loader.load(this.onAssetsLoaded.bind(this));
+
+    window.onkeydown = (ev: KeyboardEvent): any => {
+        GameApp.PressedSpace = ev.key == " ";
+      };
+  
+    Game.SetupGame();
+
+    // Ticker that runs once per frame
+    this.app.ticker.add((delta) => {
+      Game.Update(delta);
+    });
+      
+    static SetupGame() {
+          // Initial setup of the game state
     }
 
-    private onAssetsLoaded() {
-
-        const playerIdle: PIXI.AnimatedSprite = new PIXI.AnimatedSprite(playerFrames[currentFrame].map(path => PIXI.Texture.from(path)));
-
-        /*
-        * An AnimatedSprite inherits all the properties of a PIXI sprite
-        * so you can change its position, its anchor, mask it, etc
-        */
-        playerIdle.x = 100;
-        playerIdle.y = 150;
-        playerIdle['vx'] = 1;
-        playerIdle.anchor.set(0, 1);
-        // playerIdle.anchor.set(0.5);
-        playerIdle.animationSpeed = 0.3;
-        playerIdle.play();
-
-        this.app.stage.addChild(playerIdle);
+    static Update(delta: number) {
+        // simulate game, update entities and world
+        
+        // frame ending, PressedSpace back to false
+        GameApp.PressedSpace = flase;
+        
     }
-
+  }
 }
